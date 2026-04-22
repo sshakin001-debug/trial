1850b1a5695f345db343fc55d9821fd4cb163250
Arkin Shakin <sshakin001@gmail.com>
2026-04-22 10:50:34 +0600

    dino

--- zoedepth/__init__.py
+++ zoedepth/__init__.py
@@ -1,11 +1,7 @@
-"""
-ZoeDepth: Zero-shot Metric Depth Estimation
-Embedded version for trial repo - no external pip install required.
-"""
+# MIT License
+# Copyright (c) 2022 Intelligent Systems Lab Org
 
 from zoedepth.models.builder import build_model
 from zoedepth.utils.config import get_config
-
-__version__ = "0.1.0"
 
 __all__ = ['build_model', 'get_config']
--- zoedepth/models/base_models/dpt_dinov2/__init__.py
+++ zoedepth/models/base_models/dpt_dinov2/__init__.py
@@ -1,4 +1,4 @@
-from zoedepth.models.base_models.dpt_dinov2.dpt import DepthAnythingDepthModel
-from zoedepth.models.base_models.dpt_dinov2.blocks import DPTBlock
+from zoedepth.models.base_models.dpt_dinov2.dpt import DPT_DINOv2
+from zoedepth.models.base_models.dpt_dinov2.blocks import FeatureFusionBlock, ResidualConvUnit
 
-__all__ = ['DepthAnythingDepthModel', 'DPTBlock']
+__all__ = ['DPT_DINOv2', 'FeatureFusionBlock', 'ResidualConvUnit']
